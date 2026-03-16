c28b747b80d93ec89fbae3dac0cb2d8f76f4575f
megalester <mcardlek450@gmail.com>
2026-03-16 21:46:23 +0000

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const Page = () => {
-  const [mounted, setMounted] = useState(false);
+  const [mounted, setMounted] = useState(true);
   const router = useRouter();
 
   // useEffect(() => {
